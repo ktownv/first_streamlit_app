9ee16186c9613ab4d74adf058b1bcf6d8c83fb10
ktownv <kurtosis@kurtosis.org>
2022-09-23 10:00:50 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
+
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach, and Arugula Smoothie')
 streamlit.text('Hard-boiled Free-Range Egg')
 streamlit.text('🥑Avocado Toast')
+
+streamlit.header('Build your own Fruit Smoothie')
