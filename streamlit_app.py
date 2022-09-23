461cb40aaa0f6f9bf02a1f1b6e60a9357f1b756e
ktownv <kurtosis@kurtosis.org>
2022-09-23 13:14:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,7 @@ streamlit.text('🥑\N{bread}Avocado Toast')
 streamlit.header('Build your own Fruit Smoothie')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
